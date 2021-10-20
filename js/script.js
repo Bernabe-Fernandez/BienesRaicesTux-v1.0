bfd50fb325784538eb206365e709b90abe441c70
Bernabe Fernandez <bernabefernandezg05@gmail.com>
2021-10-20 16:09:48 -0500

    se agrego el js y css para la interaccion al crear una cuenta

--- js/script.js
+++ js/script.js
@@ -36,3 +36,61 @@
     });
   }
 })(document);
+
+// funcion para mostrar dependiendo de el tipo de registro vendedor o venta
+function tipochange(sel) {
+  if (sel.value == "1") {
+    //comparacion si el valor del select es 1 o 2
+    //los ids si es 1 estaran en none ocultos
+    txtinput = document.getElementById("RFCvent");
+    txtinput.style.display = "none";
+
+    txtinput = document.getElementById("celvent");
+    txtinput.style.display = "none";
+
+    txtlabel = document.getElementById("RFClabel");
+    txtlabel.style.display = "none";
+
+    txtlabel = document.getElementById("celebal");
+    txtlabel.style.display = "none";
+
+    txtinput = document.getElementById("direccionvent");
+    txtinput.style.display = "none";
+
+    txtlabel = document.getElementById("direccionlebal");
+    txtlabel.style.display = "none";
+  } else {
+    //en caso contrario estaran visibles
+    txtinput = document.getElementById("RFCvent");
+    txtinput.style.display = "block";
+
+    txtinput = document.getElementById("celvent");
+    txtinput.style.display = "block";
+
+    txtlabel = document.getElementById("RFClabel");
+    txtlabel.style.display = "block";
+
+    txtlabel = document.getElementById("celebal");
+    txtlabel.style.display = "block";
+
+    txtinput = document.getElementById("direccionvent");
+    txtinput.style.display = "block";
+
+    txtlabel = document.getElementById("direccionlebal");
+    txtlabel.style.display = "block";
+  }
+}
+
+// funcion mostrar o ocultar contraseña
+function mostrar() {
+  //vriable donde guardamos el tipo de elemento que es el id pasword
+  var tipo = document.getElementById("password");
+  //condicion para saber si el tipo es password
+  if (tipo.type === "password") {
+    //si lo es el tipo lo cambiamos a text
+    tipo.type = "text";
+  } else {
+    //sino se conserva en tipo pasword
+    tipo.type = "password";
+  }
+}
